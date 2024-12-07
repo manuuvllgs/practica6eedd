972726c09b3438a4b2c2273d1286cb661dab5bbd
DanielDelSalto <danijabalcuz@gmail.com>
2024-12-07 17:35:21 +0100

    Lo que quieras

---
+++ cmake-build-debug/CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Universidad/Practicas_Clion/Segundo/1erCuatrimestre/EstructurasDeDatos/practica6eedd/Coche.cpp" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/Coche.cpp.obj" "gcc" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/Coche.cpp.obj.d"
+  "D:/Universidad/Practicas_Clion/Segundo/1erCuatrimestre/EstructurasDeDatos/practica6eedd/PuntoRecarga.cpp" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/PuntoRecarga.cpp.obj" "gcc" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/PuntoRecarga.cpp.obj.d"
+  "D:/Universidad/Practicas_Clion/Segundo/1erCuatrimestre/EstructurasDeDatos/practica6eedd/Reanelcar.cpp" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/Reanelcar.cpp.obj" "gcc" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/Reanelcar.cpp.obj.d"
+  "D:/Universidad/Practicas_Clion/Segundo/1erCuatrimestre/EstructurasDeDatos/practica6eedd/Trayecto.cpp" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/Trayecto.cpp.obj" "gcc" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/Trayecto.cpp.obj.d"
+  "D:/Universidad/Practicas_Clion/Segundo/1erCuatrimestre/EstructurasDeDatos/practica6eedd/UTM.cpp" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/UTM.cpp.obj" "gcc" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/UTM.cpp.obj.d"
+  "D:/Universidad/Practicas_Clion/Segundo/1erCuatrimestre/EstructurasDeDatos/practica6eedd/Usuario.cpp" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/Usuario.cpp.obj" "gcc" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/Usuario.cpp.obj.d"
+  "D:/Universidad/Practicas_Clion/Segundo/1erCuatrimestre/EstructurasDeDatos/practica6eedd/fecha.cpp" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/fecha.cpp.obj" "gcc" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/fecha.cpp.obj.d"
+  "D:/Universidad/Practicas_Clion/Segundo/1erCuatrimestre/EstructurasDeDatos/practica6eedd/main.cpp" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/main.cpp.obj" "gcc" "CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Pr4_VillegasAceituno_Manuel.
+# This may be replaced when dependencies are built.
