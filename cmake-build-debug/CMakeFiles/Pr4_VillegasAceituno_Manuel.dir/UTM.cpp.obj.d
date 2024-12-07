6b675aac5632592b8d8d44905b35d06b559ec6c8
DanielDelSalto <danijabalcuz@gmail.com>
2024-12-07 17:36:15 +0100

    Revert "Lo que quieras"
    This reverts commit 972726c09b3438a4b2c2273d1286cb661dab5bbd.

--- cmake-build-debug/CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/UTM.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/UTM.cpp.obj: \
- D:\Universidad\Practicas_Clion\Segundo\1erCuatrimestre\EstructurasDeDatos\practica6eedd\UTM.cpp \
- D:\Universidad\Practicas_Clion\Segundo\1erCuatrimestre\EstructurasDeDatos\practica6eedd\UTM.h
--- cmake-build-debug/CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Pr4_VillegasAceituno_Manuel.
-# This may be replaced when dependencies are built.
