d75cee2c190f91bc2fc8b3b3cbdaf6579dbf04f3
DanielDelSalto <danijabalcuz@gmail.com>
2024-12-07 17:39:03 +0100

    Reapply "Lo que quieras"
    This reverts commit 6b675aac5632592b8d8d44905b35d06b559ec6c8.

---
+++ cmake-build-debug/CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/UTM.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/Pr4_VillegasAceituno_Manuel.dir/UTM.cpp.obj: \
+ D:\Universidad\Practicas_Clion\Segundo\1erCuatrimestre\EstructurasDeDatos\practica6eedd\UTM.cpp \
+ D:\Universidad\Practicas_Clion\Segundo\1erCuatrimestre\EstructurasDeDatos\practica6eedd\UTM.h
